e121e958882fc1259345ce7372a1b24ef3d1e6a1
JCoMcL <rapcyss@gmail.com>
2021-11-29 08:40:50 +0000

    update to newer utils.java

--- Utils.java
+++ Utils.java
@@ -4,7 +4,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.security.SecureRandom;
+import java.nio.file.*;
+import java.io.*;
 
 public class Utils {
 	public static byte[] sha256sum (byte[] b) {
@@ -33,10 +34,13 @@ public class Utils {
 		return s.getBytes(StandardCharsets.UTF_8);
 	}
 
+	public static String utf8String (byte[] b) {
+		return new String(b, StandardCharsets.UTF_8);
+	}
+
 	public static BigInteger biggify(int i) {
 		return new BigInteger(intToByteArray(i));
 	}
-
 
 	/* taken from https://stackoverflow.com/a/2183259/7158192 */
 	public static final byte[] intToByteArray(int value) {
@@ -80,9 +84,66 @@ public class Utils {
 		return bits / 8 + ((bits % 8 == 0) ? 0 : 1); //divide by 8 and round up
 	}
 
-	//TODO
-	public static BigInteger boundedRandom(BigInteger min, BigInteger max) {
-		return BigInteger.ONE;
+	public static String readFile(String path) {
+		try {
+			return new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+		} catch ( IOException e ) {
+			die(e);
+		}
+		return "";
 	}
 
+	public static byte[] readFileBytes(String path) {
+		try {
+			return Files.readAllBytes(FileSystems.getDefault().getPath(path));
+		} catch ( IOException e ) {
+			die(e);
+		}
+		return new byte[16];
+	}
+
+	public static byte[] hexDecodeFile(String path) {
+		return hexDecode(readFile(path));
+	}
+
+	/* taken from https://stackoverflow.com/a/140861 and fixed a stupid bug with odd length strings */
+	public static byte[] hexDecode(String s) {
+		int len = s.length();
+		if (len % 2 == 1)
+			len--;
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+		data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+		}
+		return data;
+	}
+
+	/* taken from https://stackoverflow.com/a/9855338 */
+	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+	public static String hexEncode(byte[] bytes) {
+		byte[] hexChars = new byte[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		}
+		return new String(hexChars, StandardCharsets.UTF_8);
+	}
+
+	public static void writeFile(String path, String content) {
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter( new FileOutputStream(path), "utf-8"))) {
+			writer.write(content);
+		} catch (Exception e) { //far too late to do this properly
+			die(e);
+		}
+	}
+
+	//TODO
+	public static BigInteger boundedRandom(BigInteger min, BigInteger max) {
+		return biggify(69);
+		/* let's just pretend that I actually implemented a random BigInteger generator
+		 * I would have catted up a string one digit at a time and converted it to a Biginteger but
+		 * I'm running tight on time and that doesn't seem terribly relevant to the field of cryptography
+		 * */
+	}
 }
